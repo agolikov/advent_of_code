d8494774f856fa3a5586f1544afbf9ef76b5271b
Alexander Golikov <a.holikau1@gmail.com>
2024-12-05 13:18:51 +0100

    change kotlin template

--- 2024/kotlin/src/Main.kt
+++
@@ -1,7 +0,0 @@
-import day05.day05_first_second
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
-    day05_first_second();
-}
